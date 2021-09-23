b8ff7db8801166d8c2b40b49ed49fcc3853a9fd6
tiger3610 <tiger3610.j@gmail.com>
2021-09-23 11:29:32 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -14,21 +14,8 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
-			if(($text == "รถแบบผมเข้าพิกัดเสียภาษีหรือไหมครับ?") || ($text == "รถแบบนี้เข้าพิกัดเสียภาษีหรือไหมครับ?") ){
-				$number = rand(0,1);
-				if($number == 0){
-					$reply_message = 'อยู่พิกัดการเสียภาษี (ผลิต/ดัดแปลงสภาพรถยนต์หรือยานพาหนะ)'; 
-				}else{
-					$reply_message = 'ไม่อยู่พิกัดการเสียภาษี!!!'; 
-				}
-				
-			}else{
-				$reply_message = 'ฉันได้รับข้อความ "'. $text.'" ของคุณแล้ว!';
-			}			
-		} else {
-			$reply_message = 'ฉันได้รับ "'.$event['message']['type'].'" ของคุณแล้ว!';
-		}
-		
+			$reply_message = 'ฉันได้รับ "'.$text.'" ของคุณแล้ว!';		
+		} 		
 	} else {
 		$reply_message = 'ฉันได้รับ Event "'.$event['type'].'" ของคุณแล้ว!';
 	}
